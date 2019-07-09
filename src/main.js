43a1849c52496a323aa461d8b154bd0e31442c0a
TillKok <Cty960904@163.com>
2019-07-09 17:40:31 +0800

    登录-页面布局

--- src/main.js
+++ src/main.js
@@ -3,16 +3,23 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'amfe-flexible'
-import {
-  Button,
-  Tabbar,
-  TabbarItem
-} from 'vant'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
-Vue
-  .use(Button)
-  .use(Tabbar)
-  .use(TabbarItem)
+Vue.use(Vant)
+/*
+后面在配置为按需引入(减少代码打包体积)
+*/
+// import {
+//   Button,
+//   Tabbar,
+//   TabbarItem
+// } from 'vant'
+
+// Vue
+//   .use(Button)
+//   .use(Tabbar)
+//   .use(TabbarItem)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,60 @@
 <template>
-    <div>login 页面</div>
+    <div class="login-wrap">
+      <!-- 头部 -->
+      <!-- 导航栏 -->
+      <van-nav-bar title="登录" class="login-head"/>
+      <!-- 导航栏结束 -->
+
+      <!-- 表单 -->
+      <van-cell-group>
+        <van-field
+          v-model="user.mobile"
+          required
+          clearable
+          label="手机号"
+          placeholder="请输入手机号"
+        />
+
+        <van-field
+          v-model="user.code"
+          type="password"
+          label="密码"
+          placeholder="请输入密码"
+          required
+        />
+      </van-cell-group>
+      <!-- 按钮 -->
+      <div class="login-btn-box">
+         <van-button type="info" class="login-btn">登录</van-button>
+      </div>
+      <!-- 按钮结束 -->
+      <!-- 表单结束 -->
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'LoginIndex'
+  name: 'LoginIndex',
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
+
 }
 </script>
 
-<style scoped></style>
+<style lang="less" scoped>
+.login-head{
+  background-color: blueviolet;
+}
+.login-btn-box{
+  padding: 20px;
+  .login-btn{
+    width: 100%;
+  }
+}
+</style>
